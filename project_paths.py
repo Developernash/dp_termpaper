0bfadba50a59e1e8cab659a437a4a784df692eb0
Nashwan Ghali Al-Zyadi <qmv203@alumni.ku.dk>
2026-06-02 11:17:00 +0200

    Final run of everything and it runs!!

--- project_paths.py
+++ project_paths.py
@@ -10,11 +10,11 @@ RESULTS_DIR = DIR / "4. Results"
 FIRST_STAGE_RESULTS_DIR = RESULTS_DIR / "first_stage estimation"
 STRUCTURAL_RESULTS_DIR = RESULTS_DIR / "Structural estimation"
 SIM_RESULTS_DIR = RESULTS_DIR / "Simulation"
+COMPARISON_DIR = RESULTS_DIR / "Comparison"
 
 FIRST_STAGE_DIR = DIR / "first_stage"
 
 PLOTS_DIR = DIR / RESULTS_DIR / "plots"
 SIM_PLOTS_DIR = PLOTS_DIR / "simulation"
-COMP_PLOTS_DIR = PLOTS_DIR / "Comparison"
 
 MODEL_FUNCTIONS_DIR = DIR / "0. Functions"
